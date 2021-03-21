302d404621db38ccbe6197248dff3c1f4250418f
Gean Araujo <icrazy.gean@gmail.com>
2021-03-21 18:02:50 -0300

    path error

--- pages/Login.js
+++ pages/Login.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 
 import styles from "../styles/Home.module.css";
-import { Header } from "../components/Header";
+import { Header } from "../../components/Header";
 
 export default function Login() {
   return (
